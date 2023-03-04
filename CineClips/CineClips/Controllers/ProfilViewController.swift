fd2c3b4cb56337f7dae2e94d00bff2d49c4ccf97
Oleksandr Chernets <set1>
2023-03-05 00:22:39 +0200

    added ProfilViewController

---
+++ CineClips/CineClips/Controllers/ProfilViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfilViewController.swift
+//  CineClips
+//
+//  Created by Alexandr Chernets on 04.03.2023.
+//
+
+import UIKit
+
+class ProfilViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
