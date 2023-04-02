e07af0f0594eceafdf8085b16f39bdb204f36278
Oleksandr Chernets <sanchupas94@gmail.com>
2023-04-02 15:19:13 +0300

    update application

---
+++ CineClips/CineClips/Application/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  CineClips
+//
+//  Created by Alexandr Chernets on 14.01.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    }
+}
